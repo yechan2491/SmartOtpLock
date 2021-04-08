87a0897caa5496078ca8a15bda2fafb45b218986
yechan2491 <yechan249124912491@gmail.com>
2021-04-08 12:40:15 +0900

    lee - test commit

--- app/src/main/java/com/cookandroid/smartotplock/create_id1.java
+++ app/src/main/java/com/cookandroid/smartotplock/create_id1.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class create_id1 extends AppCompatActivity { // commit first test
     ImageButton backBtn; //yechan - test commit2
-    Button nextBtn;
+    Button nextBtn;  //lee
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
