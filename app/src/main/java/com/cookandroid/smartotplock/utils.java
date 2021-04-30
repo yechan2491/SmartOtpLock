1f994aa8ed06ece8e1c9efdbd0b88b4564a7d813
yechan <yechan249124912491@gmail.com>
2021-04-30 16:24:46 +0900

    otp_check 자바 xml 파일 수정 - 현재 인텐트 경로지정은 안했고 작성만한 상태

---
+++ app/src/main/java/com/cookandroid/smartotplock/utils.java
@@ -0,0 +1,31 @@
+package com.cookandroid.smartotplock;
+
+import android.app.Activity;
+import android.os.Build;
+
+import androidx.core.content.ContextCompat;
+
+/***상태바의 색상을 바꾸기 위한 자바파일****/
+public class utils {
+    public enum StatusBarColorType{
+        BLACK_STATUS_BAR( R.color.black ),
+        BLUE_STATUS_BAR( R.color.statusBarColorBlue );
+
+        private int backgroundColorId;
+
+        StatusBarColorType(int backgroundColorId){
+            this.backgroundColorId = backgroundColorId;
+        }
+
+        public int getBackgroundColorId() {
+            return backgroundColorId;
+        }
+
+    }
+
+    public static void setStatusBarColor(Activity activity, StatusBarColorType colorType) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().setStatusBarColor(ContextCompat.getColor(activity, colorType.getBackgroundColorId()));
+        }
+    }
+}
