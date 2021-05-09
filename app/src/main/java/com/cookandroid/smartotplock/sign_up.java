4d693a69480250af964357403df08280685419ba
yechan2491 <yechan2491@naver.com>
2021-05-09 10:47:56 +0900

    Retrofit 설정 및 환경추가

--- app/src/main/java/com/cookandroid/smartotplock/sign_up.java
+++ app/src/main/java/com/cookandroid/smartotplock/sign_up.java
@@ -3,10 +3,17 @@ package com.cookandroid.smartotplock;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,8 +21,16 @@ import com.kakao.sdk.auth.LoginClient;
 import com.kakao.sdk.auth.model.OAuthToken;
 //import com.kakao.sdk.template.model.Button;
 
+import java.util.HashMap;
+import java.util.List;
+
 import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class sign_up extends AppCompatActivity {
     TextView sign_up_text;
@@ -25,11 +40,15 @@ public class sign_up extends AppCompatActivity {
     //private TextView nickName;
     //private ImageView profileImage;
 
+    private EditText idEditText,pwEditText;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+
 
         sign_up_text=(TextView)findViewById(R.id.sign_up_text);
 
@@ -102,6 +121,64 @@ public class sign_up extends AppCompatActivity {
                 }
             }
         });
+
+        //////////////여기부터 레트로핏 관련 코드//////////////////////////////
+        idEditText=(EditText)findViewById(R.id.idEditText);
+        pwEditText=(EditText)findViewById(R.id.pwEditText);
+
+        Retrofit retrofit =new Retrofit.Builder()
+                .baseUrl("http://jsonplaceholder.typicode.com") //여기는 서버 URL 정의
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitAPI retrofitAPI =retrofit.create(RetrofitAPI.class);
+
+        HashMap<String, Object> input =new HashMap<>();
+        input.put("userId",1);
+        input.put("title","title title");
+        input.put("body","body body 당근 당근");
+
+        retrofitAPI.postData(input).enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(response.isSuccessful()){
+                    Post data=response.body();
+                    Log.d("TEST","POST 성공성공");
+                    Log.d("TEST",data.getBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+            }
+        });
+
+
+        retrofitAPI.getData("1").enqueue( new Callback<List<Post>>(){    //id 1에 대한 행 정보 다 가져온다고 생각하면됨
+            @Override
+            public void onResponse(@NonNull Call<List<Post>> call, @NonNull Response<List<Post>> response) {
+                if(response.isSuccessful()){
+                    List<Post> data=response.body();
+                    Log.d("TEST","성공성공");
+                    Log.d("TEST",data.get(0).getTitle());   // 가져온 행에 정보중에서 "title" 정보출력
+                    idEditText.setText(data.get(0).getTitle());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+
+
+
+
+
+
+
     }
 
 
