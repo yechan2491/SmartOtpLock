958381f4b7fcc9dd7a0f0f93c2544d00e935149d
ddoongi43 <haeunlee@naver.com>
2021-05-17 22:12:46 +0900

    핀 숫자버튼 눌림효과 추가

--- app/src/main/java/com/cookandroid/smartotplock/pinVerificationActivity.java
+++ app/src/main/java/com/cookandroid/smartotplock/pinVerificationActivity.java
@@ -2,6 +2,7 @@ package com.cookandroid.smartotplock;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
