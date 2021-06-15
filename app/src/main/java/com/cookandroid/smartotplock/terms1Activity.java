ff1906bdfe45d3397eb3e2049b00d32c9a819418
ddoongi43 <haeunlee@naver.com>
2021-06-15 12:19:21 +0900

    서비스약관 - 이용약관, 개인정보 동의서 추가

--- app/src/main/java/com/cookandroid/smartotplock/terms1Activity.java
+++ app/src/main/java/com/cookandroid/smartotplock/terms1Activity.java
@@ -3,12 +3,21 @@ package com.cookandroid.smartotplock;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class terms1Activity extends AppCompatActivity {
     ImageButton backBtn;
+    TextView termText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +30,23 @@ public class terms1Activity extends AppCompatActivity {
                 finish();
             }
         });
+
+        termText = (TextView) findViewById(R.id.termText);
+        try {
+            StringBuffer data = new StringBuffer();
+            InputStream inputStream = getResources().openRawResource(R.raw.serviceterm1);
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream));
+            String str = buffer.readLine();
+            while (str != null) {
+                data.append(str + "\n");
+                str = buffer.readLine();
+            }
+            termText.setText(data);
+            buffer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "파일없음", Toast.LENGTH_SHORT).show();
+        }
     }
 }
